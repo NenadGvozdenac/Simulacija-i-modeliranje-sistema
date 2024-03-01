55f3d16f73c84e6ed2e0985156e968e65f1ea23c
NenadGvozdenac <gvozdeninenad03@gmail.com>
2024-03-01 18:28:32 +0100

    commit1: added owner model.

---
+++ Model/Owner/Location.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookingApp.Model.Owner;
+
+public class Location
+{
+    public string City { get; set; }
+    public string Country { get; set; }
+}
---
+++ Repository/Owner/AccommodationRepository.cs
@@ -0,0 +1,69 @@
+﻿using BookingApp.Model;
+using BookingApp.Serializer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BookingApp.Model;
+using BookingApp.Serializer;
+using BookingApp.Model.Vlasnik;
+
+namespace BookingApp.Repository;
+
+class AccommodationRepository
+{
+    private const string FilePath = "../../../Resources/Data/accommodations.csv";
+
+    private readonly Serializer<Accomodation> _serializer;
+
+    private List<Accomodation> _accommodations;
+
+    public AccommodationRepository()
+    {
+        _serializer = new Serializer<Accomodation>();
+        _accommodations = _serializer.FromCSV(FilePath);
+    }
+
+    public List<Accomodation> GetAll()
+    {
+        return _accommodations;
+    }
+
+    public Accomodation GetById(int id)
+    {
+        return _accommodations.FirstOrDefault(a => a.Id == id);
+    }
+
+    public void Add(Accomodation accommodation)
+    {
+        _accommodations.Add(accommodation);
+        _serializer.ToCSV(FilePath, _accommodations);
+    }
+
+    public void Update(Accomodation accommodation)
+    {
+        var existingAccommodation = _accommodations.FirstOrDefault(a => a.Id == accommodation.Id);
+        if (existingAccommodation != null)
+        {
+            existingAccommodation.Name = accommodation.Name;
+            existingAccommodation.Location = accommodation.Location;
+            existingAccommodation.Type = accommodation.Type;
+            existingAccommodation.MaxGuestNumber = accommodation.MaxGuestNumber;
+            existingAccommodation.MinReservationDays = accommodation.MinReservationDays;
+            existingAccommodation.CancellationPeriodDays = accommodation.CancellationPeriodDays;
+            existingAccommodation.Images = accommodation.Images;
+            _serializer.ToCSV(FilePath, _accommodations);
+        }
+    }
+
+    public void Delete(int id)
+    {
+        var existingAccommodation = _accommodations.FirstOrDefault(a => a.Id == id);
+        if (existingAccommodation != null)
+        {
+            _accommodations.Remove(existingAccommodation);
+            _serializer.ToCSV(FilePath, _accommodations);
+        }
+    }
+}

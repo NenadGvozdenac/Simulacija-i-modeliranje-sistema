d70771faea78a9bef8b92702402177e99412cc83
Mihajlo Sremac <mihajlosremac02@gmail.com>
2024-03-09 16:22:36 +0100

    Added few filtrations

--- View/GuestViews/Accommodations.xaml.cs
+++ View/GuestViews/Accommodations.xaml.cs
@@ -5,7 +5,9 @@ using BookingApp.View.GuestViews.Components;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
@@ -24,7 +26,7 @@ namespace BookingApp.View.GuestViews
     /// <summary>
     /// Interaction logic for Accommodations.xaml
     /// </summary>
-    public partial class Accommodations : UserControl
+    public partial class Accommodations : UserControl, INotifyPropertyChanged
     {
 
         public ObservableCollection<Accommodation> _accommodations { get; set; }
@@ -38,13 +40,18 @@ namespace BookingApp.View.GuestViews
             DataContext = this;
 
             _accommodations = new ObservableCollection<Accommodation>();
+            _filteredAccommodations = new ObservableCollection<Accommodation>();
 
             accomodationrepository = new AccommodationRepository();
             locationrepository = new LocationRepository();
             accommodationimagerepository = new AccommodationImageRepository();
 
-
             Update();
+            
+            //SearchAccommodation
+            FilteredAccommodations = new ObservableCollection<Accommodation>(_accommodations);
+            _searchAccommodation = string.Empty;
+            //
         }
 
         public void Update() 
@@ -59,5 +66,80 @@ namespace BookingApp.View.GuestViews
             }
         }
 
+
+        ////FILTERS FOR SEARCHING ACCOMMODATIONS
+        //
+        private void FilterAccommodations()
+        {
+            //Filtering Types
+            var checkedTypes = new List<string>();
+            if (checkBoxApartment.IsChecked == true)
+                checkedTypes.Add("Apartment");
+            if (checkBoxHouse.IsChecked == true)
+                checkedTypes.Add("House");
+            if (checkBoxShack.IsChecked == true)
+                checkedTypes.Add("Shack");
+
+
+
+            if (checkedTypes.Count == 0 && string.IsNullOrWhiteSpace(_searchAccommodation))
+            {
+                FilteredAccommodations = _accommodations;
+            }
+            else
+            {
+                FilteredAccommodations = new ObservableCollection<Accommodation>(
+                    _accommodations.Where(accommodation =>
+                    (checkedTypes.Count == 0 || checkedTypes.Contains(accommodation.Type.ToString())) && (string.IsNullOrWhiteSpace(_searchAccommodation) || accommodation.Name.ToLower().Contains(_searchAccommodation.ToLower()))));
+            }
+        }
+
+        private string _searchAccommodation;
+        public string SearchAccommodation
+        {
+            get { return _searchAccommodation; }
+            set
+            {
+                if(_searchAccommodation != value)
+                {
+                    _searchAccommodation = value;
+                    FilterAccommodations();
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        private ObservableCollection<Accommodation> _filteredAccommodations;
+        public ObservableCollection<Accommodation> FilteredAccommodations
+        {
+            get { return _filteredAccommodations; }
+            set
+            {
+                if (_filteredAccommodations != value)
+                {
+                    _filteredAccommodations = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private void CheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            FilterAccommodations();
+        }
+
+        private void CheckBox_Unchecked(object sender, RoutedEventArgs e)
+        {
+            FilterAccommodations();
+        }
+
+        //
+        ////END OF FILTERING
     }
 }

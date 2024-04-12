13b7c840ad74d5b1b56ffe0b7757b66c509aa389
AleksandarCvijetic <acacvijetic02@gmail.com>
2024-04-12 18:52:46 +0200

    Tour rating implemented

---
+++ Model/MutualModels/TourReview.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BookingApp.Serializer;
+
+
+namespace BookingApp.Model.MutualModels
+{
+
+    public class TourReview : ISerializable
+    {
+        public int Id { get; set; }
+        public int UserId {  get; set; }
+        public int TourId {  get; set; }
+        public int ReservationId {  get; set; }
+        public int GuideKnowledge {  get; set; }
+        public int GuideLanguage {  get; set; }
+        public int TourInterestingness {  get; set; }
+        public string Feedback {  get; set; }
+        public List<TourReviewImage> ReviewImages { get; set; }
+
+        public TourReview() { }
+        public TourReview(int userId, int tourId, int guideKnowledge, int guideLanguage, int interestingness, string feedback)
+        {
+            UserId = userId;
+            TourId = tourId;
+            //ReservationId = reservationId;
+            GuideKnowledge = guideKnowledge;
+            GuideLanguage = guideLanguage;
+            TourInterestingness = interestingness;
+            Feedback = feedback;
+        }
+
+        public void FromCSV(string[] values)
+        {
+            Id = int.Parse(values[0]);
+            UserId = int.Parse(values[1]);
+            TourId = int.Parse(values[2]);
+            //ReservationId = int.Parse(values[3]);
+            GuideKnowledge = int.Parse(values[3]);
+            GuideLanguage = int.Parse(values[4]);
+            TourInterestingness = int.Parse(values[5]);
+            Feedback = values[6];  
+        }
+
+        public string[] ToCSV()
+        {
+            return new string[] { Id.ToString(), UserId.ToString(), TourId.ToString(),/* ReservationId.ToString(),*/ GuideKnowledge.ToString(), GuideLanguage.ToString(), TourInterestingness.ToString(), Feedback };
+        }
+    }
+}

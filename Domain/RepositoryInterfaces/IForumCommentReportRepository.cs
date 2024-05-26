d47ddd0b5e74c4fe1b772eab2f21f9517a4cce1b
NenadGvozdenac <nenadgvozdenacsrb@gmail.com>
2024-05-26 11:40:20 +0200

    finished reports.

---
+++ Domain/RepositoryInterfaces/IForumCommentReportRepository.cs
@@ -0,0 +1,18 @@
+﻿using BookingApp.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookingApp.Domain.RepositoryInterfaces;
+
+public interface IForumCommentReportRepository
+{
+    public void Add(ForumCommentReport forumCommentReport);
+    public void Update(ForumCommentReport forumCommentReport);
+    public List<ForumCommentReport> GetAll();
+    public ForumCommentReport GetById(int id);
+    public List<ForumCommentReport> GetByForumCommentId(int id);
+    public List<ForumCommentReport> GetByUserId(int id);
+}
--- WPF/Views/OwnerViews/Components/ForumCommentControl.xaml.cs
+++ WPF/Views/OwnerViews/Components/ForumCommentControl.xaml.cs
@@ -1,4 +1,5 @@
-﻿using BookingApp.Domain.Models;
+﻿using BookingApp.Application.UseCases;
+using BookingApp.Domain.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -19,13 +20,36 @@ namespace BookingApp.WPF.Views.OwnerViews.Components;
 public partial class ForumCommentControl : UserControl
 {
     private readonly ForumComment comment;
+    private readonly User activeUser;
+
     public string Username => comment.User.Username;
     public string Comment => comment.Content;
 
-    public ForumCommentControl(ForumComment comment)
+    public List<ForumCommentReport> reports;
+    public string NumberOfReports => reports.Count > 0 ? string.Format("{0}🎌", reports.Count) : "";
+
+    public ForumCommentControl(ForumComment comment, User activeUser, bool EnableReportButton)
     {
         InitializeComponent();
         this.comment = comment;
+        this.activeUser = activeUser;
         DataContext = this;
+        ReportButton.Visibility = EnableReportButton ? Visibility.Visible : Visibility.Collapsed;
+        ReportCountText.Visibility = comment.User.Type == BookingApp.Resources.Types.UserType.Guest ? Visibility.Visible : Visibility.Collapsed;    
+
+        reports = ForumCommentReportService.GetInstance().GetByForumCommentId(comment.Id);
+    }
+
+    private void ReportButton_MouseDown(object sender, MouseButtonEventArgs e)
+    {
+        ForumCommentReportService.GetInstance().Add(new ForumCommentReport()
+        {
+            ForumCommentId = comment.Id,
+            UserId = UserService.GetInstance().GetById(activeUser.Id).Id
+        });
+
+        reports = ForumCommentReportService.GetInstance().GetByForumCommentId(comment.Id);
+        ReportCountText.Text = string.Format("{0}🎌", reports.Count);
+        ReportButton.Visibility = Visibility.Collapsed;
     }
 }
